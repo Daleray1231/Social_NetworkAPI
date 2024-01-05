a83b414d2c153a2fc2a36241a5262bbcdd61e686
Daleray1231 <daleray1231@gmail.com>
2024-01-05 11:09:39 -0600

    added more models

---
+++ models/Reaction.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const reactionSchema = new mongoose.Schema({
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280 
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  toJSON: {
+    getters: true
+  },
+  id: false
+});
+
+module.exports = reactionSchema;

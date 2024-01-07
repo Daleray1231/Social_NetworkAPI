b29058ff2c43bf973d0aae80893daa306df54939
Daleray1231 <daleray1231@gmail.com>
2024-01-07 16:43:22 -0600

    added comments to server.js

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
+// Import required modules
 const express = require('express');
 const mongoose = require('mongoose');
 
+// Create an Express application
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // Define the port number
 
+// Import route handlers for users and thoughts
 const userRoutes = require('./routes/userRoutes');
 const thoughtRoutes = require('./routes/thoughtRoutes');
 
-app.use('/api', thoughtRoutes);
-app.use('/api', userRoutes);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Set up middleware for parsing JSON and URL-encoded data
+app.use('/api', thoughtRoutes); // Use thought routes for '/api/thoughts' endpoints
+app.use('/api', userRoutes); // Use user routes for '/api/users' endpoints
+app.use(express.json()); // Parse JSON data
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded data
 
+// Connect to MongoDB database
 mongoose.connect('mongodb://127.0.0.1:27017/socialNetworkDB');
-mongoose.set('debug', true);
+mongoose.set('debug', true); // Enable debugging for MongoDB queries
 
+// Start the server and listen on the defined port
 app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
